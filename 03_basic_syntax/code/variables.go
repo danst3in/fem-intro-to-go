d70ca4b6dbeceb2fc0ee6dd5db46cf0624dd9209
danst3in <50937807+danst3in@users.noreply.github.com>
2020-04-26 22:35:07 -0700

    notes on syntax. create exc Control Structures

---
+++ 03_basic_syntax/code/variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// var name, name2 = "Beyonce","Lizzo"
+func main() {
+	// var name string
+	// var someNumber int
+	// shorthand style to declare variable with type
+	name := "Beyonce"
+	fmt.Println(name)
+}
