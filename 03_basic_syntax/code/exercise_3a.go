5f9a67c00fd51b82618587d5e3c8a84943a530e0
danst3in <50937807+danst3in@users.noreply.github.com>
2020-04-27 15:07:35 -0700

    refactor exercise 3a

--- 03_basic_syntax/code/exercise_3a.go
+++ 03_basic_syntax/code/exercise_3a.go
@@ -1,11 +1,12 @@
 package main
 
+/*
 import (
 	"fmt"
 )
 
-func main() {
-	var testSentence = "This is new and weird"
+ func main() {
+	var testSentence = "This is new and weird."
 	// create a variable to store a slice of bytes equal 
 	// to the characters in the string
 	b := []byte(testSentence)
@@ -19,6 +20,21 @@ func main() {
 		
 			// fmt.Println(c)
 			fmt.Println("The Index", idx, "Character(s)", c)
+		}
+	}
+} */
+
+//refactor below!
+import "fmt"
+
+func main() {
+
+	mySentence := "A testy sentence."
+
+	for idx,char := range mySentence{
+		if idx%2>0{
+
+			fmt.Println(string(char))
 		}
 	}
 }
