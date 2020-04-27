1bac585f33cfe997824d9bd65bb6e21031179df5
danst3in <50937807+danst3in@users.noreply.github.com>
2020-04-26 23:08:40 -0700

    Solved exercise 3A. Found good resource on digital ocean for type conversion  https://www.digitalocean.com/community/tutorials/how-to-convert-data-types-in-go

---
+++ 03_basic_syntax/code/exercise_3a.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var testSentence = "This is new and weird"
+	// create a variable to store a slice of bytes equal 
+	// to the characters in the string
+	b := []byte(testSentence)
+	fmt.Println(b)
+	for idx, char := range b {
+		if idx%2 > 0 {
+			// convert each byte int to a string 
+			c := string(char)
+		
+			// fmt.Println(char)
+		
+			// fmt.Println(c)
+			fmt.Println("The Index", idx, "Character(s)", c)
+		}
+	}
+}
