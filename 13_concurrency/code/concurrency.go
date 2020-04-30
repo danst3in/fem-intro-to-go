1e21af3ad278a8e630cff05f3801bd5757eefcaa
danst3in <50937807+danst3in@users.noreply.github.com>
2020-04-29 21:37:51 -0700

    Quick example of concurrent threads/async

--- 13_concurrency/code/concurrency.go
+++ 13_concurrency/code/concurrency.go
@@ -2,17 +2,39 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func say(s string) {
+// func say(s string) {
+// 	for i := 0; i < 3; i++ {
+// 		fmt.Println(s)
+// 		time.Sleep(time.Millisecond * 300)
+// 	}
+// }
+
+var wg sync.WaitGroup
+
+func handlePanic() {
+	if r := recover(); r != nil {
+		fmt.Println("PANIC")
+	}
+}
+
+func printStuff() {
+	defer wg.Done()
+	defer handlePanic()
 	for i := 0; i < 3; i++ {
-		fmt.Println(s)
+		fmt.Println(i)
 		time.Sleep(time.Millisecond * 300)
 	}
 }
 
 func main() {
-	say("Hello")
-	say("There")
+	wg.Add(1)
+	go printStuff()
+	wg.Wait()
+
+	// go say("Hello")
+	// say("There")
 }
